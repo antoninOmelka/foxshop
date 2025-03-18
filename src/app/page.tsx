34d7ee4f38db7c7216ef8908cbb46f6eb65598a9
Antonin Omelka <antonin.omelka@email.cz>
2025-03-18 22:35:00 +0100

    Search support added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,24 @@
 
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
-import { getAllProducts } from "./services/products";
+import { getProducts } from "./services/products";
 import { Product } from "@/types/product";
 import ProductItem from "@/components/ProductItem/ProductItem";
 
 export default function Home() {
+  const [nameInput, setNameInput] = useState<string>("");
+  const [minStockInput, setMinStockInput] = useState<string>("");
+  const [maxStockInput, setMaxStockInput] = useState<string>("");
+  const [includeInactiveInput, setIncludeInactive] = useState<string>("");
 
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const data = await getAllProducts();
+        const data = await getProducts();
         setProducts(data);
-      } catch(error) {
+      } catch (error) {
         console.error("Error fetching products:", error);
       }
     }
@@ -24,8 +28,58 @@ export default function Home() {
   }, []);
 
 
+  const onSearch = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const nameQuery = nameInput.trim() || undefined;
+    const minStockQuery = minStockInput ? Number(minStockInput) : undefined;
+    const maxStockQuery = maxStockInput ? Number(maxStockInput) : undefined;
+    const includeInactiveQuery = includeInactiveInput === "true" ? true : includeInactiveInput === "false" ? false : undefined;
+
+    const data = await getProducts(nameQuery, minStockQuery, maxStockQuery, includeInactiveQuery);
+    setProducts(data);
+  };
+
   return (
     <div className={styles.page}>
+      <form className={styles.searchForm} onSubmit={onSearch}>
+        <div className={styles.searchInput}>
+          <label htmlFor="name">Name</label>
+          <input id="name"
+            type="text"
+            value={nameInput}
+            onChange={(e) => setNameInput(e.target.value)}
+          />
+        </div>
+        <div className={styles.searchInput}>
+          <label htmlFor="minStock">Minimal Stock</label>
+          <input id="minStock"
+            type="number"
+            value={minStockInput}
+            onChange={(e) => setMinStockInput(e.target.value)}
+          />
+        </div>
+        <div className={styles.searchInput}>
+          <label htmlFor="maxStock">Maximal Stock</label>
+          <input id="maxStock"
+            type="number"
+            value={maxStockInput}
+            onChange={(e) => setMaxStockInput(e.target.value)}
+          />
+        </div>
+        <div className={styles.searchInput}>
+          <label htmlFor="includeActive">Include Active</label>
+          <select id="inlcudeActive"
+            value={includeInactiveInput}
+            onChange={(e) => setIncludeInactive(e.target.value)}
+          >
+            <option value={undefined}></option>
+            <option value="true">True</option>
+            <option value="false">False</option>
+          </select>
+        </div>
+        <button type="submit">Search</button>
+      </form>
       <main className={styles.main}>
         {products.map((product: Product, index) => {
           return <ProductItem key={product.name + index} name={product.name} price={product.price} stockQuantity={product.stockQuantity} />
--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -2,8 +2,16 @@ import { Product } from "@/types/product";
 
 const BASE_URL = "http://localhost:3000";
 
-export async function getAllProducts(): Promise<Product[]> {
-    const response = await fetch(`${BASE_URL}/products`);
+export async function getProducts(name?: string, minStock?: number, maxStock?: number, includeInactive?: boolean): Promise<Product[]> {
+    const params = new URLSearchParams();
+    Object.entries({ name, minStock, maxStock, includeInactive }).forEach(([key, value]) => {
+        if (value !== undefined) {
+            params.append(key, value.toString());
+        }
+    });
+  
+    const url = `${BASE_URL}/products?${params.toString()}`;
+    const response = await fetch(url);
 
     if (!response.ok) {
         throw new Error(`Error fetching products: ${response.statusText}`);
