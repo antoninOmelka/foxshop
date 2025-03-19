cf97313cb8a5a190791eb86a2c2a9f3ebd589502
Antonin Omelka <antonin.omelka@email.cz>
2025-03-19 11:22:03 +0100

    Edit support for EditableProductItem

--- src/components/EditableProductItem/EditableProductItem.tsx
+++ src/components/EditableProductItem/EditableProductItem.tsx
@@ -1,18 +1,29 @@
 import styles from "./EditableProductItem.module.css"
 import { Product } from "@/types/product";
+import { useState } from "react";
 
 
-const EditableProductItem = (product: Product) => {
-    const {id, name, price, stockQuantity} = product;
+const EditableProductItem = ({ id, name, price, stockQuantity }: Product) => {
+    const [nameInput, setNameInput] = useState(name);
+    const [priceInput, setPriceInput] = useState(price);
+    const [quantityInput, setQuantityInput] = useState(stockQuantity);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
     return (
         <div className={styles.productItem}>
             <h1>Product #{id}</h1>
-            <div className={styles.productItemDetails}>
-                <span>{`Name: ${name}`}</span>
-                <span>{`Price: ${price}`}</span>
-                <span>{`Stock quanity: ${stockQuantity}`}</span>
-                <button>Save</button>
-            </div>
+            <form className={styles.productItemDetails} onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" value={nameInput} onChange={(e) => setNameInput(e.target.value)}></input>
+                <label htmlFor="price">Price</label>
+                <input id="price" type="number" value={priceInput} onChange={(e) => setPriceInput(Number(e.target.value))}></input>
+                <label htmlFor="quantity">Stock Quantity</label>
+                <input id="quantity" type="number" value={quantityInput} onChange={(e) => setQuantityInput(Number(e.target.value))}></input>
+                <button type="submit">Save</button>
+            </form>
         </div>
     )
 }
